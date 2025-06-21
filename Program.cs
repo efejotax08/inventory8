bc174587c91da888b82abc58ffe787a4772a7862
Efejotax <fquishpe6288@uta.edu.ec>
2025-06-20 20:03:43 -0500

    First New Commit

--- Program.cs
+++ Program.cs
@@ -10,15 +10,24 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API v1");
+    c.RoutePrefix = "swagger";
+});
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapGet("/", () => "La API está corriendo correctamente en Azure");
 
 app.MapControllers();
 
