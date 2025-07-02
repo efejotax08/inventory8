5699a932b5c0cf976c6ef9d65024c3313357ff0a
Efejotax <fquishpe6288@uta.edu.ec>
2025-07-02 18:46:20 -0500

    Merge

---
+++ Controllers/CallbackController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace inventory8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CallbackController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] string oauth_token, [FromQuery] string oauth_verifier)
+        {
+            // Aquí puedes registrar temporalmente los valores para probar
+            return Ok(new
+            {
+                Message = "OAuth callback received",
+                Token = oauth_token,
+                Verifier = oauth_verifier
+            });
+        }
+    }
+}
