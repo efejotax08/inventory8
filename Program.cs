f47d9fa5e67a5652c5590aa5d2fdec09a2b041bb
Efejotax <fquishpe6288@uta.edu.ec>
2025-07-02 21:10:36 -0500

    Authorization Clever Cloud Token

--- Program.cs
+++ Program.cs
@@ -12,49 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 /*Section: Clever Cloud oAuth*/
 // Configura autenticación
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = "CleverCloud";
-})
-.AddCookie()
-.AddOAuth("CleverCloud", options =>
-{
-    options.ClientId = builder.Configuration["CleverCloud:ClientId"]; // Mejor en appsettings.json o secrets
-    options.ClientSecret = builder.Configuration["CleverCloud:ClientSecret"];
-    options.CallbackPath = new PathString("/signin-clevercloud");
-
-    options.AuthorizationEndpoint = "https://www.clever-cloud.com/oauth/authorize";
-    options.TokenEndpoint = "https://www.clever-cloud.com/oauth/token";
-    options.UserInformationEndpoint = "https://api.clever-cloud.com/v2/self";
-
-    options.Scope.Add("user:read");
-
-    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-
-    options.SaveTokens = true;
-
-    options.Events = new OAuthEvents
-    {
-        OnCreatingTicket = async context =>
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, options.UserInformationEndpoint);
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-            var response = await context.Backchannel.SendAsync(request);
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Error al obtener información del usuario de Clever Cloud");
-            }
-
-            var json = await response.Content.ReadAsStringAsync();
-            var user = JsonDocument.Parse(json);
-
-            context.RunClaimActions(user.RootElement);
-        }
-    };
-});
+builder.Services.AddHttpClient();
 /**/
 // Configura la variable de entorno con la ruta del archivo JSON
 var credentialsPath = Path.Combine(AppContext.BaseDirectory, "Credentials", "t-gateway-464020-n0-c2f9ef363c39.json");
